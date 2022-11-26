e716b3b9a982899d7e0cb25089b7e9319787aa55
tumeda <trevor.umeda@gmail.com>
2022-11-26 12:31:02 -0800

    Adding week plan object

--- cook-vue/src/App.vue
+++ cook-vue/src/App.vue
@@ -13,6 +13,34 @@ import AddRecipe from "@/components/AddRecipe.vue";
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/list">Recipes</RouterLink>
       </nav>
+      <main>
+        <div class="row">
+          <div class="col-8">
+            <h3>Draggable table</h3>
+
+            <table class="table table-striped">
+              <thead class="thead-dark">
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Sport</th>
+              </tr>
+              </thead>
+              <draggable v-model="list" tag="tbody" item-key="name">
+                <template #item="{ element }">
+                  <tr>
+                    <td scope="row">{{ element.id }}</td>
+                    <td>{{ element.name }}</td>
+                    <td>{{ element.sport }}</td>
+                  </tr>
+                </template>
+              </draggable>
+            </table>
+          </div>
+
+          <rawDisplayer class="col-3" :value="list" title="List" />
+        </div>
+      </main>
     </div>
   </header>
 
@@ -46,6 +74,22 @@ export default defineComponent({
 });
 </script>
 <style scoped>
+.draggable-list {
+  background: #3f51b5;
+  color: #fff;
+  border: 1px solid;
+  height: 50vh;
+}
+.list-item {
+  margin: 10px;
+  padding: 40px;
+  cursor: pointer;
+  font-size: 18px;
+  border-radius: 5px;
+  background: #f44336;
+  display: inline-block;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
--- cook-vue/src/views/HomeView.vue
+++ cook-vue/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import WeekPlan from "@/components/WeekPlan.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <WeekPlan />
   </main>
 </template>
